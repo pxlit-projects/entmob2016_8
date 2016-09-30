76d9696b5c1e52d7b1375d2504b4ef998247e0cf
BrechtMorrhey <brechtabc@hotmail.com>
2016-09-30 15:30:54 +0200

    SessionRestController

---
+++ Spring REST API/spring/src/main/java/be/pxl/spring/controller/SessionRestController.java
@@ -0,0 +1,22 @@
+package be.pxl.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import be.pxl.spring.model.Session;
+
+@RestController
+@RequestMapping(value="/session", produces="application/json")
+public class SessionRestController {
+
+	@Autowired
+	private SessionDao dao;
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public Session getSessionById(){
+		
+		return dao.getSessionById();
+		
+		
+	}
+}
---
+++ Spring REST API/spring/src/main/java/be/pxl/spring/dao/SessionDao.java
@@ -0,0 +1,5 @@
+package be.pxl.spring.dao;
+
+public class SessionDao {
+
+}
