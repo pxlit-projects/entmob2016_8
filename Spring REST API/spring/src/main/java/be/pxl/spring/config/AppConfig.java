dc2b5c4ca535af24e1ccbb257f58300b846433a6
BrechtMorrhey <brechtabc@hotmail.com>
2016-09-30 11:23:31 +0200

    Changed Bean Scope

--- Spring REST API/spring/src/main/java/be/pxl/spring/config/AppConfig.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/config/AppConfig.java
@@ -8,22 +8,27 @@ import be.pxl.spring.model.*;
 public class AppConfig {
 
 	@Bean
+	@Scope("prototype")
 	public Session session(){
 		return new Session();
 	}
 	@Bean
+	@Scope("prototype")
 	public User user(){
 		return new User();
 	}
 	@Bean
+	@Scope("prototype")
 	public FacebookUser facebookUser(){
 		return new FacebookUser();
 	}
 	@Bean
+	@Scope("prototype")
 	public SteamUser steamUser(){
 		return new SteamUser();
 	}
 	@Bean
+	@Scope("prototype")
 	public TwitterUser twitterUser(){
 		return new TwitterUser();
 	}
