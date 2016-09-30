c253f49c53d51c438dd4fc08472333effd76ea93
BrechtMorrhey <brechtabc@hotmail.com>
2016-09-30 14:23:18 +0200

    Spring Boot Starter Set up Correctly

--- Spring REST API/spring/src/main/java/be/pxl/spring/config/AppConfig.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/config/AppConfig.java
@@ -1,9 +1,11 @@
 package be.pxl.spring.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages={"be.pxl.spring.model"})
 public class AppConfig {
 
 }
--- Spring REST API/spring/src/main/java/be/pxl/spring/model/User.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/model/User.java
@@ -8,5 +8,32 @@ public class User {
 	
 	int userId;
 	String firstName, lastName, password, department;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public int getUserId() {
+		return userId;
+	}
 
 }
