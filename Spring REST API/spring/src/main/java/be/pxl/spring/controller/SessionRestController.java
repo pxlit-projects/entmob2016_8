91d433aac2a400fafcdbceadf5221045295084ca
BrechtMorrhey <brechtabc@hotmail.com>
2016-09-30 15:31:53 +0200

    Added MissingImport to SessionRestController

--- Spring REST API/spring/src/main/java/be/pxl/spring/controller/SessionRestController.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/controller/SessionRestController.java
@@ -3,6 +3,7 @@ package be.pxl.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import be.pxl.spring.dao.SessionDao;
 import be.pxl.spring.model.Session;
 
 @RestController
--- Spring REST API/spring/src/main/java/be/pxl/spring/dao/SessionDao.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/dao/SessionDao.java
@@ -1,5 +1,12 @@
 package be.pxl.spring.dao;
 
+import be.pxl.spring.model.Session;
+
 public class SessionDao {
+
+	public Session getSessionById() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
