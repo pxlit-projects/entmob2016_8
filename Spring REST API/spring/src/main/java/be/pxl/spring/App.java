52bed70981cad90481a6cde4396b50d1485514fd
BrechtMorrhey <brechtabc@hotmail.com>
2016-10-07 16:03:41 +0200

    UserService

--- Spring REST API/spring/src/main/java/be/pxl/spring/App.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/App.java
@@ -1,24 +1,30 @@
 package be.pxl.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.*;
 
 import be.pxl.spring.config.*;
+import be.pxl.spring.controller.UserRestController;
 import be.pxl.spring.domain.User;
+import be.pxl.spring.repository.UserRepository;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	ConfigurableApplicationContext ctx = SpringApplication.run(AppConfig.class, args);
-    	User u = new User();
-    	u.setFirstName("brecht");
-        System.out.println( u.getFirstName() );
-        
-    	System.out.println("TEST");
-    }
+public class App {
+	
+
+	
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(
+				AppConfig.class, args);
+		User u = new User();
+		u.setFirstName("brecht");
+		System.out.println(u.getFirstName());		
+		
+
+		System.out.println("TEST");
+	}
 }
---
+++ Spring REST API/spring/src/main/java/be/pxl/spring/service/UserService.java
@@ -0,0 +1,5 @@
+package be.pxl.spring.service;
+
+public class UserService {
+
+}
