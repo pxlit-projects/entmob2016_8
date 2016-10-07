15bec7fae3807c1b246124eb9cc46c3605e909eb
JasperSzkudlarski <Jasper.Szkudlarski@student.pxl.be>
2016-10-07 16:29:05 +0200

    changestosessionservice

--- Spring REST API/spring/src/main/java/be/pxl/spring/service/SessionService.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/service/SessionService.java
@@ -3,24 +3,23 @@ package be.pxl.spring.service;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Repository;
+
 import be.pxl.spring.domain.Session;
 import be.pxl.spring.domain.User;
+import be.pxl.spring.repository.SessionRepository;
 
+@Repository
+@Transactional
 public class SessionService {
 
 	private EntityManager em;
 	
+	private SessionRepository sessionRepo;
+
 	@Transactional
 	public Session save(Session session)
 	{
-		if(session.getSessionId() == 0)
-		{
-			em.persist(session);
-			return session;
-		}
-		else
-		{
-			return em.merge(session);
-		}
+		return sessionRepo.save(session);
 	}
 }
