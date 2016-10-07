3a9893c8d4ed913f9d2a2e61ad0c1da0df2d4bb5
KoenCa <koen.castermans2@student.pxl.be>
2016-10-07 11:27:12 +0200

    Models gemaakt
    kan nog veranderen

---
+++ UWP app/UWPMonitoring/UWPMonitoring.Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPMonitoring.Models
+{
+    public class User
+    {
+        public User()
+        {
+            Sessions = new List<Session>();
+        }
+
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Department { get; set; }
+
+        public List<Session> Sessions { get; set; }
+    }
+}
