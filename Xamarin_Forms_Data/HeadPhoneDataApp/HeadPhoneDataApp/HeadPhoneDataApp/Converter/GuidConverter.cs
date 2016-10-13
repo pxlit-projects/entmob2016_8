90c6b16769feb28e5f6a3b0eac91b6510a1e3c22
Stephane Oris <stephane.oris@gmail.com>
2016-10-13 15:18:42 +0200

    Nieuw xamarin project
    ViewLocator, ViewModelLocator, Adapter instelling, Main page toegevoegd

---
+++ Xamarin_Forms_Data/HeadPhoneDataApp/HeadPhoneDataApp/HeadPhoneDataApp/Converter/GuidConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HeadphoneDataApp.Converter
+{
+    public class GuidConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            Guid guid = (Guid)value;
+            return guid.ToString();
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            throw new NotImplementedException("GuidConverter is one-way");
+        }
+    }
+}
