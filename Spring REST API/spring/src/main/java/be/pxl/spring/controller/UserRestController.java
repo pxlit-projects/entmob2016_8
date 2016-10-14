ed378840d2a40918a1d149fd10ffc0dc91628c07
BrechtMorrhey <brechtabc@hotmail.com>
2016-10-14 14:14:43 +0200

    Consistente errors :dancer:

--- Spring REST API/spring/src/main/java/be/pxl/spring/controller/UserRestController.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/controller/UserRestController.java
@@ -6,28 +6,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import be.pxl.spring.domain.User;
-import be.pxl.spring.service.UserService;
+import be.pxl.spring.repository.UserRepository;
 
 @RestController
 @RequestMapping(value="/user", produces="application/json")
 public class UserRestController {
 
-	
 	@Autowired
-	UserService us;
+	UserRepository repo;
 	
-//	@RequestMapping(method = RequestMethod.GET)
-//	public User getUserById(int id){
-//		return us.findOne(id);
-//	}
-	@RequestMapping(method= RequestMethod.GET)
-	public String hello()
-	{
-		return "hello";
+	@RequestMapping(method = RequestMethod.GET)
+	public User getUserById(int id){
+		return repo.findOne(id);
 	}
 	@RequestMapping(method = RequestMethod.POST)
 	public void updateUser(User u){
-		us.save(u);
+		repo.save(u);
 	}
 	
 	
--- Spring REST API/spring/src/main/java/be/pxl/spring/repository/SessionRepository.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/repository/SessionRepository.java
@@ -1,9 +1,13 @@
 package be.pxl.spring.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import be.pxl.spring.domain.Session;
+import be.pxl.spring.domain.User;
 
 public interface SessionRepository extends JpaRepository<Session, Integer>{
+
 
 }
