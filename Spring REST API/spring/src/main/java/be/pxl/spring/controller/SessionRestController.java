c664e7658578f37750de8aa57ffb208b730e6d84
BrechtMorrhey <brechtabc@hotmail.com>
2016-10-14 14:17:25 +0200

    Revert "Consistente errors :dancer:"
    This reverts commit ed378840d2a40918a1d149fd10ffc0dc91628c07.

--- Spring REST API/spring/src/main/java/be/pxl/spring/controller/SessionRestController.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/controller/SessionRestController.java
@@ -1,32 +1,27 @@
 package be.pxl.spring.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import be.pxl.spring.domain.Session;
-import be.pxl.spring.domain.User;
 import be.pxl.spring.repository.*;
+import be.pxl.spring.service.SessionService;
 
 @RestController
 @RequestMapping(value="/session", produces="application/json")
 public class SessionRestController {
 
 	@Autowired
-	private SessionRepository repo;
+	private SessionService sessionservice;
 	
-	@RequestMapping(value="{id}", method = RequestMethod.GET)
-	public Session getSessionById(@PathVariable("id") int id){		
-		return repo.findOne(id);
+	@RequestMapping(method = RequestMethod.GET)
+	public Session getSessionById(int id){		
+		return sessionservice.findOne(id);
 		
 	}
-	
-
-	
 	@RequestMapping(method = RequestMethod.POST)
 	public void updateSession(Session s){
-		repo.save(s);
+		sessionservice.save(s);
 		
 	}
 }
--- Spring REST API/spring/src/main/java/be/pxl/spring/repository/SessionRepository.java
+++ Spring REST API/spring/src/main/java/be/pxl/spring/repository/SessionRepository.java
@@ -1,13 +1,9 @@
 package be.pxl.spring.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import be.pxl.spring.domain.Session;
-import be.pxl.spring.domain.User;
 
 public interface SessionRepository extends JpaRepository<Session, Integer>{
-
 
 }
