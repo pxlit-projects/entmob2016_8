6bb6e921dce339c309f09aa30e8d5372325c493c
Stephane Oris <stephane.oris@gmail.com>
2016-10-20 13:22:42 +0200

    ViewLocator

---
+++ Xamarin_Forms_Data/HeadphoneDataApp/HeadphoneDataApp/HeadphoneDataApp/View/ViewLocator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadphoneDataApp.View
+{
+    static class ViewLocator
+    {
+       
+        private static MainPage _mainPage;
+        private static DeviceServicePage _deviceServicePage;
+
+        public static MainPage MainPage
+        {
+            get
+            {
+                return _mainPage ?? (_mainPage = new MainPage());
+            }
+        }
+        
+        public static DeviceServicePage DeviceServicePage
+        {
+            get
+            {
+                return _deviceServicePage ?? (_deviceServicePage = new DeviceServicePage());
+            }
+        }
+    }
+}
