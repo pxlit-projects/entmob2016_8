0499c2ebe23942c6cf2bceec6e0943920e2ee78c
Stephane Oris <stephane.oris@gmail.com>
2016-10-20 13:22:28 +0200

    Converter

---
+++ Xamarin_Forms_Data/HeadphoneDataApp/HeadphoneDataApp/HeadphoneDataApp/Converters/EmptyStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HeadphoneDataApp.Converters
+{
+    public class EmptyStringConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            String str = (String)value;
+            return String.IsNullOrWhiteSpace(str) ? "<un-named device>" : str;
+        }
+
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            throw new NotImplementedException("EmptyStringConverter is one-way");
+        }
+    }
+}
