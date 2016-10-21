99715e3d99f419785325f731f0523c2d5ace5e65
Stephane Oris <stephane.oris@gmail.com>
2016-10-21 17:01:29 +0200

    Viewmodel service

--- Xamarin_Forms_Data/HeadphoneDataApp/HeadphoneDataApp/HeadphoneDataApp/ViewModel/DeviceServiceViewModel.cs
+++ Xamarin_Forms_Data/HeadphoneDataApp/HeadphoneDataApp/HeadphoneDataApp/ViewModel/DeviceServiceViewModel.cs
@@ -20,14 +20,16 @@ namespace HeadphoneDataApp.ViewModel
         private ObservableCollection<IService> services;
         private string deviceName;
 
-        
+       
+        /** Service ID. */
+        private static string ID_SERVICE = "f000aa80-0451-4000-b000-000000000000";
         /** Data UUID. */
         private static string UUID_DATA = "f000aa81-0451-4000-b000-000000000000";
         /** Configuration UUID. */
         private static string UUID_CONFIG = "f000aa82-0451-4000-b000-000000000000";
         /** Period UUID. */
         private static string UUID_PERIOD = "f000aa83-0451-4000-b000-000000000000";
-
+        private IService _accelerometerService;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -41,30 +43,61 @@ namespace HeadphoneDataApp.ViewModel
 
             this.GetServicesCommand = new Command(() =>
             {
-                if (services != null)
+                if (_accelerometerService !=null && _accelerometerService.ID.ToString() == ID_SERVICE)
                 {
-                    IService accelerometerService = null;
-                    //Zoeken naar de juiste juiste service met de acc. data
-                    foreach (var item in services)
+                    var characteristicData = _accelerometerService.Characteristics[0];
+                    var characteristicConfig = _accelerometerService.Characteristics[1];
+                    if (characteristicConfig.Uuid == UUID_CONFIG)
                     {
-                        if (item.Characteristics[0].Uuid == UUID_DATA)
+                        //enable the Accelerometer features 
+                        characteristicConfig.Write(new byte[] { 0x07 });
+
+                        if (characteristicData != null)
                         {
-                            accelerometerService = item;
+                            if (characteristicData.CanUpdate)
+                            {
+                                characteristicData.ValueUpdated += async (object sender, CharacteristicReadEventArgs e) =>
+                                {
+                                    /*string data = */
+                                    await GetData(e);
+                                    //var status = Decode(e.Characteristic.Value);
+                                    //Debug.WriteLine("Update: " + e.Characteristic.Value);
+                                    //Debug.WriteLine("Decoded: " + status);
+
+                                };
+
+                                //device.ServicesDiscovered -= Device_ServicesDiscovered; ;
+                                characteristicData.StartUpdates();
+                            }
                         }
                     }
-                    if (accelerometerService!=null)
+                
+                    else
                     {
-                        var x = accelerometerService.ToString();
-                        Debug.WriteLine("juiste service is gevonden");
+                        Debug.WriteLine("Geen service gevonden");
                     }
-                    
-                }
-                else
-                {
-                    Debug.WriteLine("Geen services gevonden");
                 }
             });
 
+        }
+
+        private async Task GetData(CharacteristicReadEventArgs e)
+        {
+            string status = Decode(e.Characteristic.Value);
+            Debug.WriteLine("Update: " + e.Characteristic.Value);
+            //return status;
+        }
+    
+        private string Decode(byte[] value)
+        {
+            var sensorData = value;
+            // Accelerometer sensorKXTJ9
+            int x = sensorData[0];
+            int y = sensorData[1];
+            int z = sensorData[2];
+            Debug.WriteLine("x: " + x + " y: " + y + " z:" + z);
+            string data = "x: " + x + " y: " + y + " z:" + z;
+            return data;
         }
 
         private void AdapterMessage(IAdapter obj)
@@ -95,21 +128,21 @@ namespace HeadphoneDataApp.ViewModel
             adapter.DeviceConnected += (s, e) =>
             {
                 device = e.Device; // do we need to overwrite this?
-                Debug.WriteLine("-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-");
                 //when services are discovered
-                device.ServicesDiscovered += (object se, EventArgs ea) =>
-                {
-                    Debug.WriteLine("device.ServicesDiscovered");
-                    //services = (List<IService>)device.Services;
-                    if (services.Count == 0)
-                        Device.BeginInvokeOnMainThread(() =>
-                        {
-                            foreach (var service in device.Services)
-                            {
-                                services.Add(service);
-                            }
-                        });
-                };
+                device.ServicesDiscovered += Device_ServicesDiscovered;
+                //device.ServicesDiscovered += (object se, EventArgs ea) =>
+                //{
+                //    Debug.WriteLine("device.ServicesDiscovered");
+                //    //services = (List<IService>)device.Services;
+                //    if (services.Count == 0)
+                //        Device.BeginInvokeOnMainThread(() =>
+                //        {
+                //            foreach (var service in device.Services)
+                //            {
+                //                services.Add(service);
+                //            }
+                //        });
+                //};
                 // start looking for services
                 device.DiscoverServices();
 
@@ -117,6 +150,26 @@ namespace HeadphoneDataApp.ViewModel
             
             //adapter.ConnectToDevice(device);
             
+        }
+
+        private void Device_ServicesDiscovered(object sender, EventArgs e)
+        {
+            Debug.WriteLine("device.ServicesDiscovered");
+            //services = (List<IService>)device.Services;
+            if (services.Count == 0)
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    foreach (var service in device.Services)
+                    {
+                        //services.Add(service);
+                        if (service.ID.ToString() == ID_SERVICE)
+                        {
+                            Debug.WriteLine("Found Accelerometer Service");
+                            _accelerometerService = service;
+
+                        }
+                    }
+                });
         }
 
         public string DeviceName
