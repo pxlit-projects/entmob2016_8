d412e82048a3b1f6371f2936077ab5b4c2debfce
Stephane Oris <stephane.oris@gmail.com>
2016-10-24 11:37:57 +0200

    LoginViewModel

---
+++ Xamarin_Forms_Data/HeadphoneDataApp/HeadphoneDataApp/HeadphoneDataApp/ViewModel/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using HeadphoneDataApp.View;
+using Robotics.Mobile.Core.Bluetooth.LE;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HeadphoneDataApp.ViewModel
+{
+    public class LoginViewModel
+    {
+        private IAdapter adapter;
+
+        public LoginViewModel()
+        {
+            //get the adapter via messenger
+            Messenger.Default.Register<IAdapter>(this, AdapterMessage);
+
+            this.LoginCommand = new Command(async () =>
+            {
+                //TO DO
+                //login controle
+                if (adapter==null)
+                {
+                    Debug.WriteLine("Geen bluetooth adapter gevonden");
+                }
+                else
+                {
+                    await App.Current.MainPage.Navigation.PushModalAsync(ViewLocator.MainPage);
+                    Messenger.Default.Send<IAdapter>(adapter);
+                }
+                
+            });
+        }
+
+        private void AdapterMessage(IAdapter obj)
+        {
+            this.adapter = obj;
+        }
+
+        public Command LoginCommand { get; private set; }
+    }
+}
