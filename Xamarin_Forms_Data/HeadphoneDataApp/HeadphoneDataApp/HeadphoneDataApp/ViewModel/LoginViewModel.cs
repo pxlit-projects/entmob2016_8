79bc8a497fe924105520f4953221cf5163fbc993
Stephane Oris <stephane.oris@gmail.com>
2016-10-25 11:21:08 +0200

    Login view + viewmodel

--- Xamarin_Forms_Data/HeadphoneDataApp/HeadphoneDataApp/HeadphoneDataApp/ViewModel/LoginViewModel.cs
+++ Xamarin_Forms_Data/HeadphoneDataApp/HeadphoneDataApp/HeadphoneDataApp/ViewModel/LoginViewModel.cs
@@ -3,6 +3,7 @@ using HeadphoneDataApp.View;
 using Robotics.Mobile.Core.Bluetooth.LE;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,7 @@ using Xamarin.Forms;
 
 namespace HeadphoneDataApp.ViewModel
 {
-    public class LoginViewModel
+    public class LoginViewModel: INotifyPropertyChanged
     {
         private IAdapter adapter;
 
@@ -30,11 +31,45 @@ namespace HeadphoneDataApp.ViewModel
                 }
                 else
                 {
-                    await App.Current.MainPage.Navigation.PushModalAsync(ViewLocator.MainPage);
-                    Messenger.Default.Send<IAdapter>(adapter);
+                    //TO DO
+                    //if (CanLogin)
+                    {
+                        await App.Current.MainPage.Navigation.PushModalAsync(ViewLocator.MainPage);
+                        Messenger.Default.Send<IAdapter>(adapter);
+                    }
+                    
                 }
                 
             });
+        }
+
+        public bool CanLogin
+        {
+            get
+            {
+                ValidationErrors = string.Empty;
+
+                if (string.IsNullOrEmpty(Username))
+                {
+                    ValidationErrors = "Please enter a username.";
+                    OnPropertyChanged("ValidationErrors");
+                }
+                if (string.IsNullOrEmpty(Password))
+                {
+                    ValidationErrors += "\nPlease enter a password.";
+                    OnPropertyChanged("ValidationErrors");
+                }
+                return string.IsNullOrEmpty(ValidationErrors);
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,
+                    new PropertyChangedEventArgs(propertyName));
+            }
         }
 
         private void AdapterMessage(IAdapter obj)
@@ -42,6 +77,10 @@ namespace HeadphoneDataApp.ViewModel
             this.adapter = obj;
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
         public Command LoginCommand { get; private set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ValidationErrors { get; private set; }
     }
 }
