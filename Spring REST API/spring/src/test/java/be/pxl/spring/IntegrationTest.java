74fdd892bfa1448f5e22f1acdc9a89c6be779a8a
BrechtMorrhey <brechtabc@hotmail.com>
2016-10-28 16:46:22 +0200

    Dit is een disaster

--- Spring REST API/spring/src/test/java/be/pxl/spring/IntegrationTest.java
+++ Spring REST API/spring/src/test/java/be/pxl/spring/IntegrationTest.java
@@ -1,5 +1,40 @@
 package be.pxl.spring;
 
-public class IntegrationTest {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import be.pxl.spring.controller.UserRestController;
+import be.pxl.spring.domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext
+@ContextConfiguration(classes=AppConfig.class)
+public class IntegrationTest {
+	@Autowired
+	UserRestController urc;
+	
+
+	@Test
+	public void testCreateUser(){
+		ConfigurableApplicationContext ctx = SpringApplication.run(
+				AppConfig.class);
+		User u = ctx.getBean(User.class);
+		u.setFirstName("brecht");
+		u.setDepartment("test");
+		u.setLastName("morrhey");
+		u.setPassword("test");
+		u.setSalt("test");
+		u.setRole("test");		
+		int newUserId = urc.updateUser(u);
+		User u2 = urc.getUserById(newUserId).getBody();
+		Assert.assertEquals(u, u2);
+		
+	}
 }
--- Spring REST API/spring/src/test/java/be/pxl/spring/TestConfig.java
+++ Spring REST API/spring/src/test/java/be/pxl/spring/TestConfig.java
@@ -1,6 +1,7 @@
 package be.pxl.spring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import be.pxl.spring.service.SessionUtility;
