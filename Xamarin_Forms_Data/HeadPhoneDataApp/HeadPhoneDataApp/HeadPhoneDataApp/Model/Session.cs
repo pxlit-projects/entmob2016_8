84b02573eb93745979a59196f92658e2f529b151
Stephane Oris <stephane.oris@gmail.com>
2016-11-02 17:17:14 +0100

    repository + session fix

--- Xamarin_Forms_Data/HeadPhoneDataApp/HeadPhoneDataApp/HeadPhoneDataApp/Model/Session.cs
+++ Xamarin_Forms_Data/HeadPhoneDataApp/HeadPhoneDataApp/HeadPhoneDataApp/Model/Session.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,13 @@ namespace HeadphoneDataApp.Model
     {
         public int SessionId { get; set; }
 
+        [JsonProperty(PropertyName = "userId")]
         public int UserId { get; set; }
 
         public DateTime Start_Time { get; set; }
 
         public DateTime End_Time { get; set; }
 
-        public DateTime Actual_Time { get; set; }
+        public int Actual_Time { get; set; }
     }
 }
