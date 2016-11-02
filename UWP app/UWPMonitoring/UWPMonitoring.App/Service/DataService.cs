9c9850239bea7d1d7d554e47c24fa0a70b9ce913
KoenCa <koen.castermans2@student.pxl.be>
2016-11-02 13:35:01 +0100

    Het omzetten van timestamp naar datetime
    verplaatst van dataservice naar het hulp session model

--- UWP app/UWPMonitoring/UWPMonitoring.App/Service/DataService.cs
+++ UWP app/UWPMonitoring/UWPMonitoring.App/Service/DataService.cs
@@ -17,23 +17,10 @@ namespace UWPMonitoring.App.Service
             this.repository = repository;
         }
 
-        //Bron voor het omzetten: http://stackoverflow.com/questions/249760/how-to-convert-a-unix-timestamp-to-datetime-and-vice-versa
         public Session GetLastSession(int userId)
         {
             SessionWithLongs sessionLong = repository.GetLastSession(userId);
-            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
-            DateTime startTime = dtDateTime.AddMilliseconds(sessionLong.Start_Time).ToLocalTime();
-            DateTime endTime = dtDateTime.AddMilliseconds(sessionLong.End_Time).ToLocalTime();
-
-            Session session = new Session();
-            session.SessionId = sessionLong.SessionId;
-            session.UserId = sessionLong.UserId;
-            session.Start_Time = startTime;
-            session.End_Time = endTime;
-            session.Actual_Time = sessionLong.Actual_Time;
-
-            return session;
-
+            return sessionLong.ToNormalSession();
         }
     }
 }
--- UWP app/UWPMonitoring/UWPMonitoring.Domain/SessionWithLongs.cs
+++ UWP app/UWPMonitoring/UWPMonitoring.Domain/SessionWithLongs.cs
@@ -19,5 +19,22 @@ namespace UWPMonitoring.Domain
 
         [JsonProperty(PropertyName = "actualTime")]
         public int Actual_Time { get; set; }
+
+        //Bron voor het omzetten van timestamp naar datetime: http://stackoverflow.com/questions/249760/how-to-convert-a-unix-timestamp-to-datetime-and-vice-versa
+        public Session ToNormalSession()
+        {
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            DateTime startTime = dtDateTime.AddMilliseconds(this.Start_Time).ToLocalTime();
+            DateTime endTime = dtDateTime.AddMilliseconds(this.End_Time).ToLocalTime();
+
+            Session session = new Session();
+            session.SessionId = this.SessionId;
+            session.UserId = this.UserId;
+            session.Start_Time = startTime;
+            session.End_Time = endTime;
+            session.Actual_Time = this.Actual_Time;
+
+            return session;
+        }
     }
 }
