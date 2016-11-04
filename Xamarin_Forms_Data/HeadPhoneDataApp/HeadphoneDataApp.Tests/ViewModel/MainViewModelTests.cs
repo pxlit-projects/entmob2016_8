6d0ace93c5503a7ebb2036771f6a1e256f1e6b9f
Stephane Oris <stephane.oris@gmail.com>
2016-11-04 15:37:33 +0100

    Tests

--- Xamarin_Forms_Data/HeadPhoneDataApp/HeadphoneDataApp.Tests/ViewModel/MainViewModelTests.cs
+++ Xamarin_Forms_Data/HeadPhoneDataApp/HeadphoneDataApp.Tests/ViewModel/MainViewModelTests.cs
@@ -4,28 +4,49 @@ using HeadphoneDataApp.ViewModel;
 using HeadphoneDataApp.Model;
 using GalaSoft.MvvmLight.Messaging;
 using Robotics.Mobile.Core.Bluetooth.LE;
+using HeadphoneDataApp.Tests.Mocks;
 
 namespace HeadphoneDataApp.Tests
 {
     [TestFixture]
     public class MainViewModelTests
     {
-        /*[Test]
-        public void Execute_Always_SendsYourMessageType()
+
+        [Test]
+        public void Found_Device()
         {
             MainViewModel main = new MainViewModel();
+            var device = new DeviceMock();
+            main.Devices.Add(device);
+            Assert.That(main.Devices!=null);
+        }
 
-            User actual = new User();
-            Messenger.Default.Register<User>(this, t => actual = t);
+        [Test]
+        public void Keep_list_If_scanning()
+        {
+            var a = new AdapterMock();
+            a.IsScanning = true;// de IsScanning staat op true
+            //Bij het starten van de scan en de adapter is  aan het scannen dan maakt hij de list devices NIET leeg
+            MainViewModel main = new MainViewModel();
+            main.Adapter = a;
+            var device = new DeviceMock();
+            main.Devices.Add(device);
+            main.StartScanning();
+            Assert.That(main.Devices!=null);
+        }
 
-
-            main.DeviceSelectedCommand.Execute(null);
-
-
-
-            User expected =// set up your expected message ;
-            Assert.That(actual, Is.EqualTo(expected));
-        }*/
+        public void Clear_List_If_scanning()
+        {
+            var a = new AdapterMock();
+            a.IsScanning = false;// de IsScanning staat op true
+            //Bij het starten van de scan en de adapter is nog niet aan het scannen dan maakt hij de list devices leeg
+            MainViewModel main = new MainViewModel();
+            main.Adapter = a;
+            var device = new DeviceMock();
+            main.Devices.Add(device);
+            main.StartScanning();
+            Assert.That(main.Devices == null);
+        }
 
     }
 }
