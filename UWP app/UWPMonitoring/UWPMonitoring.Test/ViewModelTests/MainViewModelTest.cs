1b5867a265ad04cbb50e96b6450815238277c4e1
KoenCa <koen.castermans2@student.pxl.be>
2016-11-04 11:56:26 +0100

    Testen voor het MainViewViewModel
    done

--- UWP app/UWPMonitoring/UWPMonitoring.Test/ViewModelTests/MainViewModelTest.cs
+++ UWP app/UWPMonitoring/UWPMonitoring.Test/ViewModelTests/MainViewModelTest.cs
@@ -29,21 +29,70 @@ namespace UWPMonitoring.Test.ViewModelTests
             dataService = new DataService(repository);
             navigationService = new NavigationServiceMock();
             mainViewViewModel = new MainViewViewModel(navigationService, dataService);
-            mainViewViewModel.User.UserId = 1309;
-            mainViewViewModel.User.Password = "koen";
         }
 
+        //Test een goede login
         [Fact]
-        public void LoginTest()
+        public void GoodLoginTest()
         {
             //Arrange
             init();
+            mainViewViewModel.User.UserId = 1309;
+            mainViewViewModel.User.Password = "koen";
 
             //Act
             mainViewViewModel.LoginCommand.Execute("test");
 
             //Assert
             Assert.Equal(navigationService.Key, "Overview");
+        }
+
+        //Test van een login met een onbestaand user ID
+        [Fact]
+        public void BadLoginWrongIdTest()
+        {
+            //Arrange
+            init();
+            mainViewViewModel.User.UserId = 1;
+            mainViewViewModel.User.Password = "koen";
+
+            //Act
+            mainViewViewModel.LoginCommand.Execute("test");
+
+            //Assert
+            Assert.Equal(mainViewViewModel.Message, "Het ingevulde id bestaat niet.");
+        }
+
+        //Test van een login met een verkeerd wachtwoord
+        [Fact]
+        public void BadLoginWrongPassTest()
+        {
+            //Arrange
+            init();
+            mainViewViewModel.User.UserId = 1309;
+            mainViewViewModel.User.Password = "test";
+
+            //Act
+            mainViewViewModel.LoginCommand.Execute("test");
+
+            //Assert
+            Assert.Equal(mainViewViewModel.Message, "Dit is geen geldig wachtwoord.");
+        }
+
+        //Test van een login van een user met de role user
+        [Fact]
+        public void BadLoginWrongRoleTest()
+        {
+            //Arrange
+            init();
+            mainViewViewModel.User.UserId = 1234;
+            mainViewViewModel.User.Password = "brecht";
+
+            //Act
+            mainViewViewModel.LoginCommand.Execute("test");
+
+            //Assert
+            Assert.Equal(mainViewViewModel.Message, "Deze gebruiker heeft geen admin rechten.");
         }
     }
 }
